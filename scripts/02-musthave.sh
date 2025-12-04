b61cfade6f67b988344d645b0858657663ab4a60
shorinkiwata <fcl709@outlook.com>
2025-12-04 17:18:33 +0900

    remove fcitx5-mozc

--- scripts/02-musthave.sh
+++ scripts/02-musthave.sh
@@ -106,7 +106,7 @@ fi
 # ------------------------------------------------------------------------------
 section "Step 4/8" "Input Method (Fcitx5)"
 
-exe pacman -Syu --noconfirm --needed fcitx5-im fcitx5-rime rime-ice-pinyin-git fcitx5-mozc
+exe pacman -Syu --noconfirm --needed fcitx5-im fcitx5-rime rime-ice-pinyin-git
 
 log "Configuring Rime defaults..."
 TARGET_DIR="/etc/skel/.local/share/fcitx5/rime"
